f32a0b41437543815d6dca0e94805068ddbe4430
kavery-pm <shiny.kavery@gmail.com>
2023-02-07 07:46:36 -0300

    added the login input validation

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,7 @@
 import React, { useRef,useState } from "react";
 import { useDispatch } from "react-redux";
 import { authActions } from "../store";
-
+import refreshIcon from '../assets/img/refresh-icon.png';
 import classes from './Auth.module.css';
 
 const Auth = () => {
@@ -9,12 +9,16 @@ const Auth = () => {
   const dispatch = useDispatch();
   const userNameInput = useRef("");
 const userPasswordInput = useRef("");
+const reloadInputHandler = ()=>{
+  userNameInput.current.value = '';
+  userPasswordInput.current.value='';
+}
   const loginHandler = (event)=>{
     event.preventDefault();
     if(userNameInput.current.value === 'elearnio@gmail.com' && userPasswordInput.current.value === '123456'){
       dispatch(authActions.login());
     }else{
-seterrorMessage("The username and the password entered is incorrect");
+seterrorMessage("Please enter a valid user name and password");
     }
    
   }
@@ -31,8 +35,13 @@ seterrorMessage("The username and the password entered is incorrect");
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' ref={userPasswordInput}/>
           </div>
+          <br/>
+          <div style={{display:'flex',marginLeft:'130px'}}>
           <button onClick={loginHandler}>Login</button>
-          { errorMessage !== '' && <p>{errorMessage}</p>}
+          <img src={refreshIcon} alt='refreshIcon' onClick={reloadInputHandler} width='50px' height='50px'title='reload calculator'/>
+          </div>
+          
+          { errorMessage !== '' && <p style={{color:'red'}}>{errorMessage}</p>}
         </form>
       </section>
     </main>
